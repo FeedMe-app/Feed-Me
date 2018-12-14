36f2c6040e1140d9b417a92271665ac206e07911
Tal Adari <tal0392@gmail.com>
2018-12-14 22:14:16 +0200

    Added the new activity - generate suggestions activity

---
+++ app/src/main/java/com/example/dor/testfeedme/GenerateSuggestionsActivity.java
@@ -0,0 +1,13 @@
+package com.example.dor.testfeedme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GenerateSuggestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_suggestions);
+    }
+}
