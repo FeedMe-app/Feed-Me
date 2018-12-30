9736cdd04c25bf30ef8351a4d8062cde77e34cf0
dorlevi121 <dorlevy121@gmail.com>
2018-12-30 19:20:53 +0200

    a

--- app/src/main/java/com/example/dor/testfeedme/GenerateSuggestionsActivity.java
+++ app/src/main/java/com/example/dor/testfeedme/GenerateSuggestionsActivity.java
@@ -10,20 +10,14 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 import API.RecipeConfig;
@@ -45,37 +39,30 @@ public class GenerateSuggestionsActivity extends AppCompatActivity implements
     RegularUser userDetails;
     NavigationView navigationView;
     private Button feedMeBtn;
-    private Button addIngBtn;
-    boolean isFinishLoadingUser;
-    private List<String> ingredients;
-    private List<String> chosenIng;
     private List<Recipe> recipesToChooseFrom;
     private int currRecipeIndex;
     private ImageView im;
     private DownloadImageTask imageViewHandler;
     private TextView tv;
-    LinearLayout search;
-    LinearLayout searchHeadline;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_generate_suggestions);
-        chosenIng = new ArrayList<>();
+
         findViewById(R.id.feedMeBtn).setEnabled(false);
+
         userDetails = new RegularUser();
+
         Bundle data = getIntent().getExtras();
         userEmail= data.getString("userEmail");
+
+
         InitializeSideBarMenu();
         GetUserDetails();
-        search = findViewById(R.id.SearchLinearLayout);
-        searchHeadline = findViewById(R.id.SearchHeadlineLinearLayout);
-        InitializeButtonListener();
-        InitializeTextViewListeners();
-
     }
 
     private void GetUserDetails() {
-
         Client.The().getUserFromDatabase(userEmail, new GetDataFromFirebase() {
             @Override
             public void onCallback(RegularUser user) {
@@ -85,89 +72,20 @@ public class GenerateSuggestionsActivity extends AppCompatActivity implements
                     public void run() {
                         InitializeButtonListener();
                         addFullNameToHeaderMenu();
-                        initializePremium();
-
                     }
                 });
 
             }
         });
-
-    }
-    private void initializePremium()
-    {
-        search = findViewById(R.id.SearchLinearLayout);
-        searchHeadline = findViewById(R.id.SearchHeadlineLinearLayout);
-        if(!userDetails.isPremium())
-        {
-            search.setVisibility(View.GONE);
-            searchHeadline.setVisibility(View.GONE);
-        }
     }
 
     private void InitializeButtonListener() {
         feedMeBtn = findViewById(R.id.feedMeBtn);
         feedMeBtn.setEnabled(true);
         feedMeBtn.setOnClickListener(this);
-        initializeIngAdding();
     }
 
-    private void addIng() {
-        String s = ((AutoCompleteTextView)findViewById(R.id.IngSearch)).getText().toString();
-        if(!s.equals(""))
-            chosenIng.add(s);
-    }
 
-    private void setUtilitiesContext(){
-        Utilities.setContext(this);
-    }
-
-    private void GetAllIngredients(){
-        setUtilitiesContext();
-        this.ingredients = Utilities.getIngredients();
-    }
-
-    private void InitializeTextViewListener(int tvId, final int btnId){
-
-        ((AutoCompleteTextView)findViewById(tvId)).addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.toString().trim().length()==0 ||
-                        !ingredients.contains(s.toString().trim())){
-                    findViewById(btnId).setEnabled(false);
-                } else {
-                    findViewById(btnId).setEnabled(true);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-    }
-
-    private void InitializeTextViewListeners() {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line,
-                this.ingredients);
-
-        ((AutoCompleteTextView)findViewById(R.id.IngSearch)).setAdapter(adapter);
-        InitializeTextViewListener(R.id.IngSearch, R.id.addIngBtn);
-
-    }
-
-    private void initializeIngAdding()
-    {
-        GetAllIngredients();
-
-        addIngBtn = findViewById(R.id.addIngBtn);
-        addIngBtn.setOnClickListener(this);
-    }
     private void InitializeSideBarMenu() {
         drawerLayout = findViewById(R.id.drawer);
         aToggle = new ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close);
@@ -248,6 +166,7 @@ public class GenerateSuggestionsActivity extends AppCompatActivity implements
         yearOfBirthProfile.setText(userDetails.getYearOfBirth());
     }
 
+
     private void logOutMenu(){
         Client.The().logOutUser();
         Intent intent = new Intent(GenerateSuggestionsActivity.this, MainActivity.MainActivity.class);
@@ -255,15 +174,13 @@ public class GenerateSuggestionsActivity extends AppCompatActivity implements
 
     }
 
+
+
 	@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     private void HandleUserChooseRecipe() {
         if (recipesToChooseFrom == null || recipesToChooseFrom.size() == 0){
-            RecipeConfig config = new RecipeConfig(userDetails.getTop10FavIngredients());
             Log.i("TOP INGREDIENTS: ", userDetails.getTop10FavIngredients().toString());
-            if(chosenIng.size() > 0)
-            {
-                config.setIngredients(chosenIng);
-            }
+            RecipeConfig config = new RecipeConfig(userDetails.getTop10FavIngredients());
             if (userDetails.getAllergies().size() > 0){
                 config.setAllergies(userDetails.getAllergies());
             }
@@ -271,7 +188,6 @@ public class GenerateSuggestionsActivity extends AppCompatActivity implements
                 config.setDislikes(userDetails.getDislikes());
             }
             recipesToChooseFrom = Utilities.findRecpiesByUserPreferences(config);
-            Log.i("SIZE:", (String.valueOf(recipesToChooseFrom.size())));
         }
         StartShowingRecipes();
     }
@@ -322,10 +238,6 @@ public class GenerateSuggestionsActivity extends AppCompatActivity implements
             case R.id.feedMeBtn:
                 HandleUserChooseRecipe();
                 break;
-
-            case R.id.addIngBtn:
-                addIng();
-                ((AutoCompleteTextView)findViewById(R.id.IngSearch)).setText("");
         }
     }
 
@@ -350,7 +262,5 @@ public class GenerateSuggestionsActivity extends AppCompatActivity implements
         InitializeButtonListener();
         InitializeSideBarMenu();
         addFullNameToHeaderMenu();
-        initializePremium();
-        InitializeTextViewListeners();
     }
 }
